61ddf5c678e55de8ca4e8702f0453273ed8256a6
me <developer@dev.com>
2021-01-10 18:27:17 +0200

    Channing containers frames

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ export const devVideoSnapshotUrl = `https://images.pexels.com/photos/78783/subma
 //export const devVideoSnapshotUrl = `//camera.ehps.ncsu.edu:8100/c8`
 
 export const devVideoStreamUrl = `http://88.53.197.250/axis-cgi/mjpg/video.cgi?resolution=320x240`;
-//export const devVideoStreamtUrl = `//camera.ehps.ncsu.edu:8100/c8`;
+//export const devVideoStreamUrl = `//camera.ehps.ncsu.edu:8100/c8`;
 
 export const popupSize  = {
   small: 'small',
